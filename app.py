27571a53c9368fc848f10350b7ac1a454f02eb85
Xedrik <julioxedrik@gmail.com>
2026-07-02 15:20:28 +0800

    Added sign-up screen and route

--- app.py
+++ app.py
@@ -10,5 +10,26 @@ def home():
     return render_template('Homepage.html')
 
 
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        # Capture form values submitted by the user
+        email = request.form.get('email')
+        password = request.form.get('password')
+        account_type = request.form.get('account_type')
+        campus = request.form.get('campus')
+
+        # Capture specific volunteer details if applicable
+        volunteer_type = request.form.get(
+            'volunteer_type')  # Student/Faculty/Staff
+        id_number = request.form.get('id_number')
+
+        # lagay dito yung sqalchemy insetion logic
+
+        return redirect(url_for('home'))
+
+    return render_template('Signup.html')
+
+
 if __name__ == '__main__':
     app.run(debug=True)
